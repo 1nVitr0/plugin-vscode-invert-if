9a5e78c560f33755879d8987d0742ccbed736524
Aram Becker <becker.aram@gmail.com>
2021-09-20 22:51:52 +0200

    feat: implement condition inversion

--- src/services/ASTService.ts
+++ src/services/ASTService.ts
@@ -1,26 +1,25 @@
-import { Node, Parser } from 'acorn';
-import { generate } from 'astring';
+import { parse, types, print } from 'recast';
 import { Range, TextDocument } from 'vscode';
 import ConfigurationService from './ConfigurationService';
 
 export default class ASTService {
   public constructor(private configurationService: ConfigurationService) {}
 
-  public parse(code: string, language: string): Node {
+  public parse(code: string, language: string): types.ASTNode {
     const languageOptions =
       this.configurationService.languageOptions[language] || this.configurationService.languageOptions.default;
-    return Parser.parse(code, { ecmaVersion: languageOptions.useEcmaVersion });
+    return parse(code);
   }
 
-  public stringify(node: Node, language: string): string {
-    return generate(node);
+  public stringify(node: types.ASTNode, language: string): string {
+    return print(node).code;
   }
 
-  public parseDocument(document: TextDocument, range?: Range): Node {
+  public parseDocument(document: TextDocument, range?: Range): types.ASTNode {
     return this.parse(document.getText(range), document.languageId);
   }
 
-  public parseDocumentRange(document: TextDocument, range: Range): Node {
+  public parseDocumentRange(document: TextDocument, range: Range): types.ASTNode {
     return this.parse(document.getText(range), document.languageId);
   }
 }
--- src/services/ConditionInversionService.ts
+++ src/services/ConditionInversionService.ts
@@ -1,83 +1,79 @@
-import { Node } from 'acorn';
+import { ExpressionKind, UnaryExpressionKind, BinaryExpressionKind, LogicalExpressionKind } from 'ast-types/gen/kinds';
+import { visit } from 'ast-types';
+import { types } from 'recast';
 import ConfigurationService from './ConfigurationService';
 
-export type LogicOperator = '==' | '===' | '!=' | '!==' | '<' | '<=' | '>' | '>=' | '&&' | '||';
-export type UnaryLogicOperator = '!';
-export interface UnaryExpression extends Node {
-  operator: UnaryLogicOperator;
-  argument: Node;
-}
-export interface BinaryExpression extends Node {
-  operator: LogicOperator;
-  left: Node;
-  right: Node;
-}
+type ConditionalExpression = BinaryExpressionKind | UnaryExpressionKind | LogicalExpressionKind;
 
-export function isUnaryExpression(node: Node): node is UnaryExpression {
-  return node.type == 'UnaryExpression';
-}
+type OperatorMap<
+  K extends ExpressionKind & { operator: string },
+  V extends ExpressionKind & { operator: string } = K,
+  A = never
+> = Record<K['operator'], V['operator'] | A>;
 
-export function isBinaryExpression(node: Node): node is BinaryExpression {
-  return node.type == 'BinaryExpression' || node.type == 'LogicalExpression';
+export function isConditionalExpression(node: ExpressionKind): node is ConditionalExpression {
+  return ['BinaryExpression', 'LogicalExpression', 'UnaryExpression'].includes(node.type);
 }
 
 export default class ConditionInversionService {
-  public static inverseOperator: { [k in LogicOperator]: LogicOperator } = {
+  public static inverseOperator: Partial<OperatorMap<BinaryExpressionKind>> & OperatorMap<LogicalExpressionKind> = {
     '==': '!=',
     '===': '!==',
     '!=': '==',
     '!==': '===',
     '&&': '||',
     '||': '&&',
+    '??': '&&',
     '<': '>=',
     '<=': '>',
     '>': '<=',
     '>=': '<',
   };
-  public static inverseUnaryOperator: { [k in UnaryLogicOperator]: UnaryLogicOperator | null } = { '!': null };
+  public static inverseUnaryOperator: Partial<OperatorMap<UnaryExpressionKind, UnaryExpressionKind, null>> = {
+    '!': null,
+  };
 
   public constructor(private configurationService: ConfigurationService) {}
 
-  public extractConditions(node: Node): Node[] {
-    // TODO: implement - stub for testing:
-    return [(<any>node).body[0].test as Node];
+  public extractConditions(node: types.ASTNode): ExpressionKind[] {
+    const conditions: ExpressionKind[] = [];
+
+    visit(node, {
+      visitIfStatement: (statement) => conditions.push(statement.node.test),
+      visitForStatement: (statement) => statement.node.test && conditions.push(statement.node.test),
+      visitWhileStatement: (statement) => conditions.push(statement.node.test),
+      visitDoWhileStatement: (statement) => conditions.push(statement.node.test),
+    });
+
+    return conditions;
   }
 
-  public inverse(
-    condition: Node,
-    depth = this.configurationService.inversionDepth
-  ): Node | UnaryExpression | BinaryExpression {
-    if (isUnaryExpression(condition) || depth == 0) {
-      return this.inverseGroup(condition);
-    } else if (isBinaryExpression(condition)) {
-      const isLogic = condition.type == 'LogicalExpression';
-      const inverse = ConditionInversionService.inverseOperator[condition.operator];
-      if (!inverse) return this.inverseGroup(condition);
-      return {
-        ...condition,
-        operator: inverse,
-        left: isLogic ? this.inverse(condition.left, depth - 1) : condition.left,
-        right: isLogic ? this.inverse(condition.right, depth - 1) : condition.right,
-      };
-    } else {
-      return condition;
+  public inverse(condition: ExpressionKind, depth = this.configurationService.inversionDepth): ExpressionKind {
+    if (!isConditionalExpression(condition) || depth == 0) return this.inverseGroup(condition);
+
+    switch (condition.type) {
+      case 'UnaryExpression':
+        return this.inverseGroup(condition as types.namedTypes.UnaryExpression);
+      case 'LogicalExpression':
+        const inverseLogical = ConditionInversionService.inverseOperator[(condition as LogicalExpressionKind).operator];
+        return types.builders.logicalExpression(
+          inverseLogical,
+          this.inverse(condition.left, depth - 1),
+          this.inverse(condition.right, depth - 1)
+        );
+      case 'BinaryExpression':
+        const inverseBinary = ConditionInversionService.inverseOperator[(condition as BinaryExpressionKind).operator];
+        if (!inverseBinary) return this.inverseGroup(condition);
+        return types.builders.binaryExpression(inverseBinary, condition.left, condition.right);
     }
   }
 
-  public inverseGroup(condition: UnaryExpression | Node): Node | UnaryExpression | BinaryExpression {
-    if (!isUnaryExpression(condition)) {
-      return {
-        type: 'UnaryExpression',
-        operator: '!',
-        prefix: true,
-        start: condition.start,
-        end: condition.end,
-        argument: condition,
-      } as Node;
-    }
+  public inverseGroup(condition: ExpressionKind): ExpressionKind {
+    if (condition.type !== 'UnaryExpression') return types.builders.unaryExpression('!', condition, true);
 
     const inverse = ConditionInversionService.inverseUnaryOperator[condition.operator];
-    if (inverse) return { ...condition, operator: inverse };
-    else return condition.argument;
+    if (inverse) return types.builders.unaryExpression(inverse, condition.argument, condition.prefix);
+    else if (inverse == null) return condition.argument;
+    else return types.builders.unaryExpression('!', condition, true);
   }
 }
