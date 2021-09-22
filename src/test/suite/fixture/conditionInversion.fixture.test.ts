758e966b84278c43a9dc3f6cda1e580cb303daf1
Aram Becker <becker.aram@gmail.com>
2021-09-23 00:17:49 +0200

    test: fill in tests for if-else and condition inversion

--- src/test/suite/fixture/conditionInversion.fixture.test.ts
+++ src/test/suite/fixture/conditionInversion.fixture.test.ts
@@ -1,8 +1,25 @@
-import FixtureTestRunner from '../../helpers/FixtureTestRunner';
+import { IfStatementKind } from 'ast-types/gen/kinds';
+import LinesAndColumns from 'lines-and-columns';
+import ASTService from '../../../services/ASTService';
+import ConditionInversionService from '../../../services/ConditionInversionService';
+import ConfigurationService from '../../../services/ConfigurationService';
 import asyncSuite from '../../helpers/asyncSuite';
+import FixtureTestRunner from '../../helpers/FixtureTestRunner';
 
 asyncSuite('Fixture tests for condition inversion', async function () {
-  const suites = await FixtureTestRunner.suiteRunners('condition-inversion', async (text) => text);
+  const configurationService = new ConfigurationService();
+  const astService = new ASTService(configurationService);
+  const conditionInversionService = new ConditionInversionService(configurationService);
+
+  const suites = await FixtureTestRunner.suiteRunners('condition-inversion', async (code) => {
+    const node = astService.parse(code, 'js');
+    const condition = (node.program.body[0] as IfStatementKind).test;
+    const inverse = conditionInversionService.inverse(condition);
+    const inverseCode = astService.stringify(inverse, 'js');
+    const [start, end] = FixtureTestRunner.mapRangeToOffset(code, condition.loc?.start, condition.loc?.end);
+
+    return code.slice(0, start) + inverseCode + code.slice(end);
+  });
 
   return FixtureTestRunner.suites(suites, this);
 });
--- src/test/suite/fixture/simpleIfElse.fixture.test.ts
+++ src/test/suite/fixture/simpleIfElse.fixture.test.ts
@@ -1,8 +1,23 @@
-import FixtureTestRunner from '../../helpers/FixtureTestRunner';
+import { IfStatementKind } from 'ast-types/gen/kinds';
+import ASTService from '../../../services/ASTService';
+import ConditionInversionService from '../../../services/ConditionInversionService';
+import ConfigurationService from '../../../services/ConfigurationService';
+import IfElseInversionService from '../../../services/IfElseInversionService';
 import asyncSuite from '../../helpers/asyncSuite';
+import FixtureTestRunner from '../../helpers/FixtureTestRunner';
 
 asyncSuite('Fixture tests for simple if else inversion', async function () {
-  const suites = await FixtureTestRunner.suiteRunners('simple-if-else', async (text) => text);
+  const configurationService = new ConfigurationService();
+  const astService = new ASTService(configurationService);
+  const conditionInversionService = new ConditionInversionService(configurationService);
+  const ifElseInversionService = new IfElseInversionService(configurationService, conditionInversionService);
+
+  const suites = await FixtureTestRunner.suiteRunners('simple-if-else', async (code) => {
+    const node = astService.parse(code, 'js');
+    const inverse = ifElseInversionService.inverse(node.program.body[0] as IfStatementKind);
+
+    return astService.stringify(inverse, 'js');
+  });
 
   return FixtureTestRunner.suites(suites, this);
 });
