5b8919f80883590ab5b6cd45affd1b89849f309e
Aram Becker <becker.aram@gmail.com>
2021-09-23 13:56:36 +0200

    feat: add nyc coverage output for extension tests

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,8 +1,27 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import { join } from 'path';
 
-export function run(): Promise<void> {
+function setupCoverage() {
+  const NYC = require('nyc');
+  const nyc = new NYC({
+    cwd: join(__dirname, '..', '..', '..'),
+    reporter: ['text', 'lcov'],
+    instrument: true,
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true,
+  });
+
+  nyc.reset();
+  nyc.wrap();
+
+  return nyc;
+}
+
+export async function run(): Promise<void> {
+  const nyc = process.env.COVERAGE ? setupCoverage() : null;
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -10,24 +29,25 @@ export function run(): Promise<void> {
   });
 
   const testsRoot = path.resolve(__dirname, '..');
+  const files = glob.sync('**/**.test.js', { cwd: testsRoot });
 
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) return e(err);
+  // Add files to the test suite
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) e(new Error(`${failures} tests failed.`));
-          else c();
-        });
-      } catch (err) {
-        console.error(err);
-        e(err);
-      }
+  try {
+    // Run the mocha test
+    await new Promise<void>((c, e) => {
+      mocha.run((failures) => {
+        if (failures > 0) e(new Error(`${failures} tests failed.`));
+        else c();
+      });
     });
-  });
+  } catch (err) {
+    throw err;
+  } finally {
+    if (nyc) {
+      nyc.writeCoverageFile();
+      await nyc.report();
+    }
+  }
 }
