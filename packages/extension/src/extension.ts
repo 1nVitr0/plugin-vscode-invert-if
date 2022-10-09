8f08a8316ba616232de933ec63703487e7d6c0a2
Aram Becker <becker.aram@gmail.com>
2022-10-09 15:09:45 +0200

    fix(extension): export api in activation function

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -7,10 +7,10 @@ import { service } from "./globals";
 export function activate(context: ExtensionContext) {
   registerTypescriptInvertIfProvider(service.plugins);
   context.subscriptions.push(service, ...contributeCommands(), ...contributeCodeActions());
+
+  return service.plugins;
 }
 
 export function deactivate() {
   window.showWarningMessage("Invert If is now deactivated. You must reload the window to use it again.");
 }
-
-export default service.plugins;
