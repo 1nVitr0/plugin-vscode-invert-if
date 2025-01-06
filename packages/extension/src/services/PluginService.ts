c9c232d6b3508df03bbdca5a7d9049a823015f60
Aram Becker <aram@konsolenkost.de>
2025-01-06 10:04:30 +0100

    refactor(extension): expose helper functions of PluginService

--- packages/extension/src/services/PluginService.ts
+++ packages/extension/src/services/PluginService.ts
@@ -26,6 +26,64 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
   private registerProviderEvent: EventEmitter<Plugin<any>>;
   private unregisterProviderEvent: EventEmitter<Plugin<any>>;
 
+  public static compareDocumentSelectors(a: DocumentSelector, b: DocumentSelector, strict = true): boolean {
+    if (typeof a !== typeof b) return false;
+    if (typeof a === "object" && typeof b == "object" && "length" in a !== "length" in b) return false;
+
+    if (typeof a === "string") {
+      return a === b;
+    }
+
+    if (typeof a === "object" && "length" in a && typeof b == "object" && "length" in b) {
+      return strict
+        ? a.every((aItem) => b.some((bItem) => PluginService.compareDocumentSelectors(aItem, bItem)))
+        : a.some((aItem) => b.some((bItem) => PluginService.compareDocumentSelectors(aItem, bItem)));
+    }
+
+    const _a = a as DocumentFilter;
+    const _b = b as DocumentFilter;
+
+    return (
+      _a.language === _b.language && _a.scheme === _b.scheme && _a.pattern === _b.pattern && _a.scheme === _b.scheme
+    );
+  }
+
+  public static describePlugin(plugin: Plugin<any>): string {
+    const { capabilities, documentSelector } = plugin;
+    const documentSelectorString = JSON.stringify(documentSelector);
+    const capabilitiesString = Object.entries(capabilities)
+      .filter(([, value]) => value)
+      .map(([key]) => key)
+      .join(", ");
+
+    return `for ${documentSelectorString} with capabilities [${capabilitiesString}]`;
+  }
+
+  public static matchDocumentContext(documentSelector: DocumentSelector, context: DocumentContext): boolean {
+    if ((Array.isArray as (arg: DocumentSelector) => arg is ReadonlyArray<DocumentFilter | string>)(documentSelector)) {
+      return documentSelector.some((selector) => PluginService.matchDocumentContext(selector, context));
+    } else if (typeof documentSelector === "string") {
+      return documentSelector === context.languageId;
+    } else {
+      return (
+        documentSelector.language === context.languageId &&
+        (!documentSelector.scheme || documentSelector.scheme === "embedded")
+      );
+    }
+  }
+
+  public static compareCapabilities<T>(a: Plugin<T>["capabilities"], b: Plugin<T>["capabilities"]): number {
+    let result = 0;
+
+    // We intentionally use weak compare, as `false` and `undefined` are treated equally
+    if (a.invertCondition == b.invertCondition) result++;
+    if (a.invertIfElse == b.invertIfElse) result++;
+    if (a.guardClause == b.guardClause) result++;
+    if (a.embeddedLanguages == b.embeddedLanguages) result++;
+
+    return result;
+  }
+
   public constructor(private configurationService: ConfigurationService) {
     this.registerProviderEvent = new EventEmitter();
     this.unregisterProviderEvent = new EventEmitter();
@@ -96,17 +154,25 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     });
   }
 
-  public getAvailableCapabilities(document: TextDocument): Plugin<any>["capabilities"] {
-    const plugins = this.plugins.filter((plugin) => languages.match(plugin.documentSelector, document));
+  public getAvailableCapabilities(context: DocumentContext): Plugin<any>["capabilities"] {
+    const plugins = this.plugins.filter((plugin) =>
+      PluginService.matchDocumentContext(plugin.documentSelector, context)
+    );
     const capabilities = plugins.reduce(
       (acc, plugin) => {
         return {
           invertCondition: acc.invertCondition || plugin.capabilities.invertCondition,
           invertIfElse: acc.invertIfElse || plugin.capabilities.invertIfElse,
           guardClause: acc.guardClause || plugin.capabilities.guardClause,
+          embeddedLanguages: acc.embeddedLanguages || plugin.capabilities.embeddedLanguages,
         };
       },
-      { invertCondition: false, invertIfElse: false, guardClause: false } as Plugin<any>["capabilities"]
+      {
+        invertCondition: false,
+        invertIfElse: false,
+        guardClause: false,
+        embeddedLanguages: false,
+      } as Plugin<any>["capabilities"]
     );
 
     return capabilities;
@@ -114,11 +180,13 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
 
   public getAvailableDocumentSelector(): ReadonlyArray<DocumentFilter | string> {
     const selectors: (DocumentFilter | string)[] = [];
-    for (const plugin of this.plugins) {
-      if (plugin.documentSelector instanceof Array) {
-        selectors.push(...plugin.documentSelector);
-      } else {
-        selectors.push(plugin.documentSelector);
+    for (const { documentSelector } of this.plugins) {
+      const documentSelectors = documentSelector instanceof Array ? documentSelector : [documentSelector];
+
+      for (const selector of documentSelectors) {
+        if (!selectors.some((compare) => PluginService.compareDocumentSelectors(selector, compare))) {
+          selectors.push;
+        }
       }
     }
 
@@ -131,7 +199,7 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     return isEmbeddedDocumentContext(context)
       ? this.plugins.find(
           ({ capabilities, documentSelector }) =>
-            this.matchDocumentContext(documentSelector, context) && capabilities.invertCondition
+            PluginService.matchDocumentContext(documentSelector, context) && capabilities.invertCondition
         )?.provider
       : this.plugins.find(
           ({ capabilities, documentSelector }) =>
@@ -145,7 +213,7 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     return isEmbeddedDocumentContext(context)
       ? this.plugins.find(
           ({ capabilities, documentSelector }) =>
-            this.matchDocumentContext(documentSelector, context) && capabilities.invertIfElse
+            PluginService.matchDocumentContext(documentSelector, context) && capabilities.invertIfElse
         )?.provider
       : this.plugins.find(
           ({ capabilities, documentSelector }) =>
@@ -159,7 +227,7 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     return isEmbeddedDocumentContext(context)
       ? this.plugins.find(
           ({ capabilities, documentSelector }) =>
-            this.matchDocumentContext(documentSelector, context) && capabilities.guardClause
+            PluginService.matchDocumentContext(documentSelector, context) && capabilities.guardClause
         )?.provider
       : this.plugins.find(
           ({ capabilities, documentSelector }) =>
@@ -173,7 +241,7 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     return isEmbeddedDocumentContext(context)
       ? this.plugins.find(
           ({ capabilities, documentSelector }) =>
-            this.matchDocumentContext(documentSelector, context) && capabilities.embeddedLanguages
+            PluginService.matchDocumentContext(documentSelector, context) && capabilities.embeddedLanguages
         )?.provider
       : this.plugins.find(
           ({ capabilities, documentSelector }) =>
@@ -196,10 +264,10 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
 
   private init() {
     this.onRegisterProvider((plugin) => {
-      console.info("Invert If: Registered plugin", this.describePlugin(plugin));
+      console.info("Invert If: Registered plugin", PluginService.describePlugin(plugin));
     });
     this.onUnregisterProvider((plugin) => {
-      console.info("Invert If: Unregistered plugin", this.describePlugin(plugin));
+      console.info("Invert If: Unregistered plugin", PluginService.describePlugin(plugin));
     });
   }
 
@@ -214,7 +282,7 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
       );
     }
 
-    if (existingPlugin && this.compareDocumentSelectors(existingPlugin.documentSelector, documentSelector)) {
+    if (existingPlugin && PluginService.compareDocumentSelectors(existingPlugin.documentSelector, documentSelector)) {
       for (const key of Object.keys(capabilities) as (keyof Plugin<T>["capabilities"])[]) {
         existingPlugin.capabilities[key] = existingPlugin.capabilities[key] || capabilities[key];
       }
@@ -259,63 +327,6 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     }
   }
 
-  private describePlugin(plugin: Plugin<any>): string {
-    const { capabilities, documentSelector } = plugin;
-    const documentSelectorString = JSON.stringify(documentSelector);
-    const capabilitiesString = Object.entries(capabilities)
-      .filter(([, value]) => value)
-      .map(([key]) => key)
-      .join(", ");
-
-    return `for ${documentSelectorString} with capabilities [${capabilitiesString}]`;
-  }
-
-  private compareDocumentSelectors(a: DocumentSelector, b: DocumentSelector, strict = true): boolean {
-    if (typeof a !== typeof b) return false;
-    if (typeof a === "object" && typeof b == "object" && "length" in a !== "length" in b) return false;
-
-    if (typeof a === "string") {
-      return a === b;
-    }
-
-    if (typeof a === "object" && "length" in a && typeof b == "object" && "length" in b) {
-      return strict
-        ? a.every((aItem) => b.some((bItem) => this.compareDocumentSelectors(aItem, bItem)))
-        : a.some((aItem) => b.some((bItem) => this.compareDocumentSelectors(aItem, bItem)));
-    }
-
-    const _a = a as DocumentFilter;
-    const _b = b as DocumentFilter;
-
-    return (
-      _a.language === _b.language && _a.scheme === _b.scheme && _a.pattern === _b.pattern && _a.scheme === _b.scheme
-    );
-  }
-
-  private matchDocumentContext(documentSelector: DocumentSelector, context: DocumentContext): boolean {
-    if ((Array.isArray as (arg: DocumentSelector) => arg is ReadonlyArray<DocumentFilter | string>)(documentSelector)) {
-      return documentSelector.some((selector) => this.matchDocumentContext(selector, context));
-    } else if (typeof documentSelector === "string") {
-      return documentSelector === context.languageId;
-    } else {
-      return (
-        documentSelector.language === context.languageId &&
-        (!documentSelector.scheme || documentSelector.scheme === "embedded")
-      );
-    }
-  }
-
-  private compareCapabilities<T>(a: Plugin<T>["capabilities"], b: Plugin<T>["capabilities"]): number {
-    let result = 0;
-
-    // We intentionally use weak compare, as `false` and `undefined` are treated equally
-    if (a.invertCondition == b.invertCondition) result++;
-    if (a.invertIfElse == b.invertIfElse) result++;
-    if (a.guardClause == b.guardClause) result++;
-
-    return result;
-  }
-
   private getExistingPlugin<T>(
     provider: InvertConditionProvider<T> | InvertIfElseProvider<T> | GuardClauseProvider<T> | EmbeddedLanguageProvider
   ): Plugin<T> | undefined {
@@ -326,8 +337,8 @@ export default class PluginService implements InvertIfBaseProvider, Disposable {
     return this.plugins.find(
       (compare) =>
         plugin.provider !== compare.provider &&
-        this.compareDocumentSelectors(plugin.documentSelector, compare.documentSelector, false) &&
-        this.compareCapabilities(plugin.capabilities, compare.capabilities)
+        PluginService.compareDocumentSelectors(plugin.documentSelector, compare.documentSelector, false) &&
+        PluginService.compareCapabilities(plugin.capabilities, compare.capabilities)
     );
   }
 }
